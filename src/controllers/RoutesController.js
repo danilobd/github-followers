ef1fb17af39d5b053920ced37c1f93a139366ca5
Danilo Dutra <contato@danbd.me>
2020-10-04 17:51:50 -0300

    chore: add username form

--- src/controllers/RoutesController.js
+++ src/controllers/RoutesController.js
@@ -1,6 +1,12 @@
 class RoutesController {
   index(req, res) {
-    res.render('index', { title: 'teste', message: 'Hello Word' });
+    const urlForm = process.env.NODE_ENV === 'development' ? `http://localhost:${process.env.PORT}/api/username` : 'http://localhost/api/username';
+
+    res.render('index', {
+      title: 'GitHub Follow/Followers tracker',
+      message: 'GitHub Follow/Followers tracker',
+      urlForm,
+    });
   }
 
   status(req, res) {
