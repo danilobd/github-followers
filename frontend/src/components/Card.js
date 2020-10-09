07943cb8dc475d65912d48448c4f726f284b27a2
Danilo Dutra <contato@danbd.me>
2020-10-09 16:34:28 -0300

    refactor: change Cards color and size

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -3,10 +3,10 @@ import React from "react";
 const Card = ({element}) => {
   return (
     <div className="col p-2">
-      <div key={element.id} className="card" style={{width: 9 + 'rem'}}>
+      <div key={element.id} className="card">
         <img src={element.avatar_url} className="card-img-top" alt={element.login} />
         <div className="card-body p-1">
-          <a href={element.html_url} target="_blank" rel="noopener noreferrer" className="card-link text-dark h6">
+          <a href={element.html_url} target="_blank" rel="noopener noreferrer" className="card-link text-light h6">
             {element.login}
           </a>
         </div>
